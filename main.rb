e643c8afac9bdfc3aa90428d2ecb0910889c063e
Stesha McKindle <steshamckindle@gmail.com>
2020-07-24 22:29:40 +0000

    Apply .to_f to gets.chomp() to convert string to a float at time of user input

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 puts "Lets add some numbers!"
 puts "Enter a number: "
-num1 = gets.chomp()
+num1 = gets.chomp().to_f
 puts "Enter another number: "
-num2 = gets.chomp()
-puts (num1.to_f + num2.to_f)
+num2 = gets.chomp().to_f
+puts (num1 + num2)
